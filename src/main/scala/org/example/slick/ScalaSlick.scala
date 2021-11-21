b5e730528b304d873daadeaaaa9ed8d673e9800d
苏 <enmoton@qq.com>
2021-11-21 19:01:57 +0800

    slick

--- src/main/scala/org/example/slick/ScalaSlick.scala
+++ src/main/scala/org/example/slick/ScalaSlick.scala
@@ -14,17 +14,19 @@ object ScalaSlick extends App {
   val timeout = 10 second
 
   // define table
-  class User(tag: Tag) extends Table[(Int, String, String)](tag, "user") {
+  case class User(id: Int, username: String, password: String)
+
+  class Users(tag: Tag) extends Table[User](tag, "user") {
     def id = column[Int]("id", O.PrimaryKey)
 
     def username = column[String]("username", O.Unique, O.Length(255))
 
     def password = column[String]("password")
 
-    override def * = (id, username, password)
+    override def * = (id, username, password) <> (User.tupled, User.unapply)
   }
 
-  val users = TableQuery[User]
+  val users = TableQuery[Users]
 
   val setup = DBIO.seq(
     // drop scheme
@@ -33,18 +35,25 @@ object ScalaSlick extends App {
     users.schema.createIfNotExists,
 
     // insert
-    users += (0, "username", "password"),
+    users += User(0, "username", "password"),
 
     // insert all
     users ++= Seq(
-      (1, "username1", "password"),
-      (2, "username2", "password"),
-      (3, "username3", "password"),
-    )
+      User(1, "username1", "password"),
+      User(2, "username2", "password"),
+      User(3, "username3", "password"),
+    ),
   )
 
   // run setup
   Await.result(database.run(setup), timeout)
+
+  // delete from user where id = 4
+  Await.result(database.run(users.filter(_.id === 4).delete), timeout)
+
+  // update user set username = 'aaa' where id = 3
+  println(users.filter(_.id === 3).map(_.username).update("aaa").statements)
+  Await.result(database.run(users.filter(_.id === 3).map(_.username).update("aaa")), timeout)
 
   // select * from user where id = 0
   Await.result(database.run(users.filter(_.id === 0).result), timeout).foreach(println)
